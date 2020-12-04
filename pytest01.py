3e2b85ab2533c0e033c8fb24b80b0b2cadf0f8cb
LiuSiPan <1145691125@qq.com>
2020-12-04 16:51:41 +0800

    first commit

---
+++ pytest01.py
@@ -0,0 +1,16 @@
+
+
+
+# def testMyset(num1,num2):
+#     print("红红火火恍恍惚惚：{}....{}".format((num1+num2),"没有撤退可言"))
+#
+# testMyset(1,6)
+
+
+class Solution(object):
+    def convert(self, s, numRows):
+        print("{}{}".format(s,numRows))
+
+
+s = Solution()
+s.convert("LEETCODEISHIRING",4)
